96ce7402db9ebe5c54f92b5d97104dfbdc15ecc2
BehruzMulkamonov <126500929+BehruzMulkamonov@users.noreply.github.com>
2023-07-30 00:41:12 +0500

    clas meta

--- config/settings.py
+++ config/settings.py
@@ -121,9 +121,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = 'images/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static'
+# ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_ROOT = BASE_DIR / 'images'
 
